79e235d2de139c4938a93a49733c740f47c28dbc
Austin Seven Horning <austinseven@outlook.com>
2015-11-26 13:22:00 -0500

    Finished Bogosort

--- mucking_around/bogosort.cpp
+++ mucking_around/bogosort.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-#define SORT_RANGE 100
+#define SORT_RANGE 1000
 #define PARTITION_SIZE 10
 
 #define DATA_TAG 0
@@ -40,11 +40,11 @@ int * create_array(int size){
   return list;
 }
 
-void shuffle_array(int list[], int size){
+void shuffle_array(int * list, int size){
   int placeholder;
   int index;
   
-  for(int i = SORT_RANGE; i > 0; i--){
+  for(int i = size; i > 0; i--){
     index = std::rand() % i;
     placeholder = list[index];
     list[index] = list[i - 1];
@@ -52,18 +52,19 @@ void shuffle_array(int list[], int size){
   }
 }
 
-bool check_array(int list[], int size){
+bool check_array(int * list, int size){
   int last = list[0];
   
   for(int i = 1; i < size; i++){
     if(list[i] < last){
       return false;
     }
+    last = list[i];
   }
   return true;
 }
 
-int * merge_arrays(int list1[], int size1, int list2[], int size2){
+int * merge_arrays(int * list1, int size1, int * list2, int size2){
   int * list = new int[size1 + size2];
   int point = 0;
   int i = 0;
@@ -103,6 +104,7 @@ int * merge_arrays(int list1[], int size1, int list2[], int size2){
 
 void perform_master(){
   int * list = create_array(SORT_RANGE);
+  shuffle_array(list, SORT_RANGE);
   int comm_size = MPI::COMM_WORLD.Get_size();
   MPI_Status status;
   int * sorted_list;
@@ -113,6 +115,7 @@ void perform_master(){
   for(int i = 1; i < comm_size; i++){
     MPI_Send(list + next_to_send, PARTITION_SIZE, MPI_INT, i, DATA_TAG, MPI_COMM_WORLD);
     next_to_send += PARTITION_SIZE;
+    cout << "Node " << status.MPI_SOURCE << " sent initial stock.\n";
   }
   
   while(sorted_list_size < SORT_RANGE){
@@ -122,9 +125,12 @@ void perform_master(){
     if(next_to_send < SORT_RANGE){
       MPI_Send(list + next_to_send, PARTITION_SIZE, MPI_INT, status.MPI_SOURCE, DATA_TAG, MPI_COMM_WORLD);
       next_to_send += PARTITION_SIZE;
+      cout << "Node " << status.MPI_SOURCE << " sent restock.\n";
+      
     }
     else{
       MPI_Send(list, 1, MPI_INT, status.MPI_SOURCE, TERMINATE_TAG, MPI_COMM_WORLD);
+      cout << "Node " << status.MPI_SOURCE << " sent termination.\n";
     }
   }
   cout << sorted_list[0];
@@ -142,18 +148,21 @@ void perform_slave(int rank){
   while(true){
     MPI_Iprobe(0, TERMINATE_TAG, MPI_COMM_WORLD, &terminate_flag, MPI_STATUS_IGNORE);
     if(terminate_flag){
+      cout << "Node " << rank << " terminated.\n";
       return;
     }
     
     MPI_Iprobe(0, DATA_TAG, MPI_COMM_WORLD, &data_flag, MPI_STATUS_IGNORE);
     if(data_flag){
+      cout << "Node " << rank << " restocked.\n";
       shuffles = 0;
       MPI_Recv(data, PARTITION_SIZE, MPI_INT, 0, DATA_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      while(!check_array){
+      while(!check_array(data, PARTITION_SIZE)){
         shuffle_array(data, PARTITION_SIZE);
         shuffles++;
       }
-      cout << "Node " << rank << " sorted an array in " << shuffles << " shuffles.";
+      cout << "Node " << rank << " sorted an array in " << shuffles << " shuffles.\n";
+      MPI_Send(data, PARTITION_SIZE, MPI_INT, 0, DATA_TAG, MPI_COMM_WORLD);
     }
   }
 }
